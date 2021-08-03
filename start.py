1ccc96af66880d7110b62fbf6e59f30cb61a216d
neetsincodes <88376459+neetsincodes@users.noreply.github.com>
2021-08-03 22:22:04 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,21 @@
+num = 10
+type(num)
+print("neetika")
+print("neeitka" + " " + "Singh")
+x, y = input("enter your num: ").split()
+print(x)
+print(y)
+
+name, age = input("Enter your name and age: ").split()
+print(name)
+print(age)
+
+isRain = True
+print("raining")
+
+def alexa():
+    isworking = True
+    print("wow")
+alexa()
+
+type(isRain)
